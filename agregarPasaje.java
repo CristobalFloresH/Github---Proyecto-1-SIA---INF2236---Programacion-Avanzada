4f41b212212a7e5c7402c462edf2170c69853eb9
DeTeX360 <35928513+DeTeX360@users.noreply.github.com>
2025-08-25 22:43:22 -0400

    agregarPasaje.java

--- agregarPasaje.java
+++ agregarPasaje.java
@@ -1,20 +1,7 @@
-public static void agregarPasaje(Persona persona, int idPasaje, String horaLlegada, String horaSalida,
-        String destinoFinal, int costoPasaje, int asiento, String fecha) {
-	
+public static void agregarPasaje(Persona persona, Pasaje pasaje) {
 	if(persona.getSaldo() >= costoPasaje) {    
-		Pasaje pasaje = anadirPasaje(idPasaje, horaLlegada, horaSalida, destinoFinal, costoPasaje, asiento, fecha);
-	
-		int largo = persona.getListaPasajes().length;
-		Pasaje[] nuevosPasajes = new Pasaje[largo + 1];
-	
-		for(int i = 0; i < largo; i++) {
-			nuevosPasajes[i] = persona.getListaPasajes()[i];
-		}
-		nuevosPasajes[largo] = pasaje;
-		persona.setListaPasajes(nuevosPasajes);
-
+		Pasaje pasaje = anadirPasaje(Pasaje pasaje);
 		persona.setSaldo(persona.getSaldo() - costoPasaje);
-	
 		System.out.println("Pasaje agregado correctamente.");        
 		} else {
 		System.out.println("No tienes saldo suficiente para comprar este pasaje.");
