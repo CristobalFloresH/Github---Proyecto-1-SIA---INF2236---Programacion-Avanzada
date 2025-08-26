e0b2da7be72090978b24d28c541676429264e2dc
CristobalFloresH <cristobal.flores.h@mail.pucv.cl>
2025-08-25 21:33:50 -0400

    Update and rename PlaceHolderClass.java to Persona.java

---
+++ Clases/Persona.java
@@ -0,0 +1,49 @@
+package Package;
+
+import java.util.ArrayList;
+
+public class persona{
+	private String nombre;
+	private String rut;
+	private ArrayList<pasaje> pasajes;
+	private int saldoDisponible;
+	
+
+	public persona(String nombre, String rut, int saldoDisponible) {
+        this.nombre = nombre;
+        this.rut = rut;
+        this.pasajes = new ArrayList<>();
+        this.saldoDisponible = saldoDisponible;
+	}
+	
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public String getRut() {
+        return rut;
+    }
+
+    public ArrayList<pasaje> getPasajes() {
+        return pasajes;
+    }
+
+    public int getSaldoDisponible() {
+        return saldoDisponible;
+    }
+    
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public void setRut(String rut) {
+        this.rut = rut;
+    }
+
+    public void setSaldoDisponible(int saldoDisponible) {
+        this.saldoDisponible = saldoDisponible;
+    }
+}
+
+
