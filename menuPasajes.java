e7211aefeaaf4d9639cc42433ac9d8b2d735c8e5
DeTeX360 <35928513+DeTeX360@users.noreply.github.com>
2025-08-25 22:01:48 -0400

    Create menuPasajes.java

---
+++ menuPasajes.java
@@ -0,0 +1,18 @@
+Scanner sc = new Scanner(System.in);
+	
+System.out.println("Ingrese la accion a realizar (1-2-3-4)");
+System.out.println("1. Mostrar todos los pasajes");
+System.out.println("2. Agregar pasaje a una persona");
+System.out.println("3. Salir");
+int opcion = sc.nextInt();
+
+switch(opcion) {
+    case 1:
+    mostrarPasajes(viajes);
+	  break;
+    case 2:
+	  agregarPasaje(persona, pasaje)			
+    case 3:
+	  System.out.println("Saliendo...");
+	  break;			
+}
