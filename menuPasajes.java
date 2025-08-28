3cfcbcca704e4eedddc4b45f05a9c2c082446220
DeTeX360 <35928513+DeTeX360@users.noreply.github.com>
2025-08-28 19:19:27 -0400

    menuPasajes.java

--- menuPasajes.java
+++ menuPasajes.java
@@ -1,20 +1,23 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+
 public class menuPasajes{
-	public static void menuPasajes(String[] args) {
-		Scanner sc = new Scanner(System.in);
+	public static void main(String[] args){
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		
 		System.out.println("Ingrese la accion a realizar (1-2-3-4)");
 		System.out.println("1. Mostrar todos los pasajes");
 		System.out.println("2. Agregar pasaje a una persona");
 		System.out.println("3. Salir");
-		int opcion = sc.nextInt();
-		
+		int opcion = Integer.parseInt(br.readLine());
 		
 		switch(opcion) {
 		case 1:
 			mostrarPasajes(persona);
 			break;
 		case 2:
-			agregarPasaje(persona, viaje)		
+			agregarPasaje(persona, viaje);
 			break;
 		case 3:
 			System.out.println("Saliendo...");
