afd7a3c355e3de7f83e30b7adf676946660d6d33
CristobalFloresH <cristobal.flores.h@mail.pucv.cl>
2025-09-25 21:48:35 -0300

    Create viajeDirecto.java

---
+++ Clases/viajeDirecto.java
@@ -0,0 +1,15 @@
+package Package;
+
+public class viajeDirecto extends viaje {
+
+    public viajeDirecto(int viajeID, int costoViaje, int costoParaEmpresa, String patente, String origen, String destinoFinal, String horaSalida, String horaLlegada) {
+        super(viajeID, costoViaje, costoParaEmpresa, patente, origen, destinoFinal, horaSalida, horaLlegada);
+    }
+
+    @Override
+    public void mostrarInformacionViajeCompleta() {
+        super.mostrarInformacionViajeCompleta();
+        System.out.println("Tipo de viaje: Directo");
+        System.out.println("---------------------------------");
+    }
+}
