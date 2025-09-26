97a5c3657381cd4bc0a060eaa5653d38d9f44780
CristobalFloresH <cristobal.flores.h@mail.pucv.cl>
2025-09-25 21:48:50 -0300

    Create viajeNoDirecto.java

---
+++ Clases/viajeNoDirecto.java
@@ -0,0 +1,15 @@
+package Package;
+
+public class viajeNoDirecto extends viaje {
+
+    public viajeNoDirecto(int viajeID, int costoViaje, int costoParaEmpresa, String patente, String origen, String destinoFinal, String horaSalida, String horaLlegada) {
+        super(viajeID, costoViaje, costoParaEmpresa, patente, origen, destinoFinal, horaSalida, horaLlegada);
+    }
+
+    @Override
+    public void mostrarInformacionViajeCompleta() {
+        super.mostrarInformacionViajeCompleta();
+        System.out.println("Tipo de viaje: No Directo");
+        System.out.println("---------------------------------");
+    }
+}
