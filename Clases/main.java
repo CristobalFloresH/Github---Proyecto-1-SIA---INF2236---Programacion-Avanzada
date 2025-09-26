53a58af74d7b429dc03abbb063baf7cac82afb81
CristobalFloresH <cristobal.flores.h@mail.pucv.cl>
2025-09-25 21:47:39 -0300

    Update main.java

--- Clases/main.java
+++ Clases/main.java
@@ -5,17 +5,17 @@ import java.io.IOException;
 public class Main {
     public static void main(String[] args) throws IOException {
 
-        persona persona1 = new persona("Cristobal", "12345678-9", 25000);
+        persona persona1 = new persona("Cristobal", "12345678-9", 35000);
         persona persona2 = new persona("Diego", "98765432-1", 10000);
         persona persona3 = new persona("Mauri", "11122233-4", 2000);
 
-        bus bus1 = new bus(30, "HP*DS*20", 1, 1);
-        bus bus2 = new bus(45, "JK*TH*53", 2, 3);
-        bus bus3 = new bus(20, "LM*TG*90", 3, 2);
+        busPremium bus1 = new busPremium(30, "HP*DS*20", 1, 1);
+        busComun bus2 = new busComun(45, "JK*TH*53", 2, 3);
+        busComun bus3 = new busComun(20, "LM*TG*90", 3, 2);
 
-        viaje viaje1 = new viaje(25, 10000, 120000 ,bus1.getPatente(), "El Quisco", "Santiago", "10:30", "13:00");
-        viaje viaje2 = new viaje(40, 20000, 250000, bus2.getPatente(), "Santiago", "Quilpue", "08:30", "12:00");
-        viaje viaje3 = new viaje(15,  5000, 80000, bus3.getPatente(), "El Tabo", "San Antonio", "07:30", "08:00");
+        viajeDirecto viaje1 = new viajeDirecto(25, 10000, 120000 ,bus1.getPatente(), "El Quisco", "Santiago", "10:30", "13:00");
+        viajeNoDirecto viaje2 = new viajeNoDirecto(40, 2000, 250000, bus2.getPatente(), "Santiago", "Quilpue", "08:30", "12:00");
+        viajeNoDirecto viaje3 = new viajeNoDirecto(15, 5000, 80000, bus3.getPatente(), "El Tabo", "San Antonio", "07:30", "08:00");
 
         empresa empresaBuses = new empresa();
         
@@ -41,6 +41,7 @@ public class Main {
         persona3.comprarPasaje(viaje3, busDeViaje3, 19, "2025-07-12");
 
         miVentana frame = new miVentana(empresaBuses);
+        
         frame.setVisible(true);
     }
 }
