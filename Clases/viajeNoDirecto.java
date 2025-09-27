0943a5c4c38fe9d755ed896fb1fe8f7e08694826
CristobalFloresH <cristobal.flores.h@mail.pucv.cl>
2025-09-27 13:47:53 -0300

    Update viajeNoDirecto.java

--- Clases/viajeNoDirecto.java
+++ Clases/viajeNoDirecto.java
@@ -1,14 +1,19 @@
 package Package;
 
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+
 public class viajeNoDirecto extends viaje {
 
-    public viajeNoDirecto(int viajeID, int costoViaje, int costoParaEmpresa, String patente, String origen, String destinoFinal, String horaSalida, String horaLlegada) {
+    public viajeNoDirecto(int viajeID, int costoViaje, int costoParaEmpresa, String patente, String origen,
+                        String destinoFinal, String horaSalida, String horaLlegada) {
         super(viajeID, costoViaje, costoParaEmpresa, patente, origen, destinoFinal, horaSalida, horaLlegada);
     }
 
-    public void mostrarInformacionViajeCompleta() {
-        super.mostrarInformacionViajeCompleta();
-        System.out.println("Tipo de viaje: No Directo");
-        System.out.println("---------------------------------");
+    public JPanel infoViajeCompletaComoPanel() {
+        JPanel panel = super.infoViajeCompletaComoPanel();
+        panel.add(new JLabel("Tipo de viaje: No Directo"));
+        panel.add(new JLabel("---------------------------------"));
+        return panel;
     }
 }
