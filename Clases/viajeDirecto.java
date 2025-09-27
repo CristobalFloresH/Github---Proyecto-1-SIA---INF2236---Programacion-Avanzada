8277bb55a649876fcd13de9278c2f0e5500134a9
CristobalFloresH <cristobal.flores.h@mail.pucv.cl>
2025-09-27 13:47:28 -0300

    Update viajeDirecto.java

--- Clases/viajeDirecto.java
+++ Clases/viajeDirecto.java
@@ -1,14 +1,17 @@
 package Package;
+import javax.swing.*;
 
 public class viajeDirecto extends viaje {
 
-    public viajeDirecto(int viajeID, int costoViaje, int costoParaEmpresa, String patente, String origen, String destinoFinal, String horaSalida, String horaLlegada) {
+    public viajeDirecto(int viajeID, int costoViaje, int costoParaEmpresa, String patente, String origen,
+                        String destinoFinal, String horaSalida, String horaLlegada) {
         super(viajeID, costoViaje, costoParaEmpresa, patente, origen, destinoFinal, horaSalida, horaLlegada);
     }
 
-    public void mostrarInformacionViajeCompleta() {
-        super.mostrarInformacionViajeCompleta();
-        System.out.println("Tipo de viaje: Directo");
-        System.out.println("---------------------------------");
+    public JPanel infoViajeCompletaComoPanel() {
+        JPanel panel = super.infoViajeCompletaComoPanel();
+        panel.add(new JLabel("Tipo de viaje: Directo"));
+        panel.add(new JLabel("---------------------------------"));
+        return panel;
     }
 }
